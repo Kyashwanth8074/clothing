c2e0d825d98720e48aa1f5e4f27ee49fa49d80a1
Kyashwanth8074@gmail.com <Kyashwanth8074@gmail.com>
2021-10-24 14:57:38 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+
+import './App.css';
+
+import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component';
+import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
+import Header from './components/header/header.component';
+
+function App() {
+  return (
+    <div>
+      <h1>Project</h1>
+      <Header />
+      <Switch>
+        <Route exact path='/' component={HomePage} />
+        <Route path='/shop' component={ShopPage} />
+        <Route path='/signin' component={SignInAndSignUpPage} />
+      </Switch>
+    </div>
+  );
+}
+
+export default App;
+
+
+//$ ssh-keygen -t rsa -b 4096 -C "kyashwanth8074@gmail.com"
+
+//kyashwanth8074@gmail.com
